800e748d1aad6319ea755774cd044e60835d5644
Kimsuhhee04 <kimyesol0428@gmail.com>
2026-05-13 12:43:31 +0900

    init: 프로젝트 초기 세팅

---
+++ src/test/java/com/likelion/tripTruth/TripTruthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.tripTruth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripTruthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
